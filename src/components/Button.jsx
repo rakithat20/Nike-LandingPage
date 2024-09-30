f5cfba1a37897745ce5d9b29a23b0f67352538d9
Rakitha <tharupathir@gmail.com>
2024-09-30 23:35:30 +0530

    Changed Button and added about us

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,8 +5,8 @@ const Button = ({label,iconUrl}) => {
    gap-2 px-7 py-4 border font-montserrat text-lg leading-none
    bg-coral-red rounded-full text-white">
    {label}
-   <img src={iconUrl} className="ml-2 rounded-full w-5 h-5
-   " />
+   {iconUrl&&<img src={iconUrl} className="ml-2 rounded-full w-5 h-5
+   " />}
    </button>
   )
 }
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,4 +1,5 @@
 import Button from "../components/Button"
+import {shoe8} from "../assets/images"
 const SuperQuality = () => {
   return (
     <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
@@ -20,7 +21,13 @@ const SuperQuality = () => {
         <p className="mt-6 lg:max-w-lg info-text">
           Our dedication to detial and excellence ensures your satisfaction
         </p>
+        <div className="mt-11">
         <Button label="View Details" />
+        </div>
+      </div>
+
+      <div className="flex-1 items-center justify-center">
+        <img src={shoe8} alt="" width={570} height={522}  className="object-contain"/>
       </div>
 
     </section>

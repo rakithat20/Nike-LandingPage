7c09d396c48b69dbb77734c1427c31ec1754b00c
Rakitha <tharupathir@gmail.com>
2024-10-07 23:55:15 +0530

    Started Customer Reviews section

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,8 @@
+
+const ReviewCard = ({imgURL,}) => {
+  return (
+    <div>ReviewCard</div>
+  )
+}
+
+export default ReviewCard
--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,7 +1,20 @@
-
+import {reviews} from '../constants'
+import ReviewCard from '../components/ReviewCard'
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container flex items-center justify-center flex-col">
+      <h3 className="text-4xl font-palanquin leading-tight font-bold ">
+        What Our 
+        <span className="text-coral-red" > Customers </span>
+         Say ?
+      </h3>
+      <p className="text-slate-gray font-montserrat mt-6 text-lg text-center max-w-lg">
+      Hear genuine stories from our satisfied customers about their exceptional experiences with us.
+      </p>
+      <div className="mt-20 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review)=>(<ReviewCard imgURL={review.imgURL}/>))}
+      </div>
+    </section>
   )
 }
 

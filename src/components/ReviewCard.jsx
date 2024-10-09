d99ac483d26cadb8a3df9abd63dafa755512bdbd
Rakitha <tharupathir@gmail.com>
2024-10-09 12:48:20 +0530

    Update ReviewCard.jsx

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -2,10 +2,10 @@ import { star } from "../assets/icons"
 
 const ReviewCard = ({imgURL,customerName,rating,feedback}) => {
   return (
-    <div className="flex flex-col justify-center items-center gap-3">
+    <div className="flex flex-col justify-center items-center">
         <img src={imgURL} alt={customerName} width={124} height={124} className="rounded-full object-cover" />
         <p className="mt-6 info-text max-w-sm">{feedback}</p>
-        <div className="flex gap-4">
+        <div className="flex gap-4 mt-2">
         <img src={star} alt="" width={24} height={24} className="object-contain m-0"/><p className="text-xl font-montserrat text-slate-gray">({rating})</p>
         
         </div>
